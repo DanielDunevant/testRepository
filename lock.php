d3506834d17c265288c065e1a4e2ab26c176852d
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-09-21 18:07:14 -0500

    Ahhhh

--- lock.php
+++ lock.php
@@ -1,16 +1,19 @@
+<?php
+$siteName ="My Test Site";
+print"
 <!DOCTYPE html>
-<html lang="en">
+<html lang=\"en\">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<meta charset="UTF-8" />
-	<meta name="author" content="Novay Technologies" />
-	<title>Novay Technologies</title>
-	<link rel="shortcut icon" href="/favicon.ico" />
-	<link rel="icon" type="image/png" href="/favicon.png">
+	<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />
+	<meta charset=\"UTF-8\" />
+	<meta name=\"author\" content=\"$siteName\" />
+	<title>$siteName</title>
+	<link rel=\"shortcut icon\" href=\"/favicon.ico\" />
 </head>
 <body>
-	<h1 style="text-align:center;">Novay Technologies</h1>
-	<h2 style="text-align:center;">This site is currently under development</h2>
-	<p style="text-align:center;">If you need to contact us, feel free to do so at: support@novaytechnologies.com</p>
+	<h1 style=\"text-align:center;\">$siteName</h1>
+	<h2 style=\"text-align:center;\">This site is currently under development</h2>
+	<p style=\"text-align:center;\">If you need to contact us, feel free to do so at: support@novaytechnologies.com</p>
 </body>
-</html>
+</html>";
+?>
